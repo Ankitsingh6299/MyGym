5caf04255466ecb5d9cbc3dae087c8f2ac212682
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-09-19 00:55:34 +0530

    Header left section completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,6 +10,39 @@ function Hero() {
             <div></div>
             <span>the best fitness club in the town</span>
         </div>
+        <div className='hero-text'>
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+  
+          <div>
+            <span>Ideal Body</span>
+          </div>
+
+          <div>
+            <span>We will help you transform yourself into a better form,a more attractive version of you and will make you feel confident again!</span>
+          </div>
+        </div>
+        <div className='figures'>
+          <div>
+            <span>+140</span>
+            <span>Expert Coaches</span>
+          </div>
+          <div>
+          <span>+978</span>
+            <span>Members Joined</span>
+          </div>
+          <div>
+          <span>+50</span>
+            <span>Fitness programs</span>
+          </div>
+        </div>
+
+        <div className='hero-buttons'>
+          <button className='btn'>Get Started</button>
+          <button className='btn'>Learn More</button>
+        </div>
         </div>
         <div className='right-h'>Right Side</div>
     </div>
