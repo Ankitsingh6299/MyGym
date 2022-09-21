426c17f5d537d5d0d1c1cb8338e895d6148cf161
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-09-21 18:19:44 +0530

    Explore Programs,section and cards made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './components/Hero';
+import Program from './components/Program';
 
 function App() {
   return (
     <div className="App">
           <Hero />
+          <Program />
     </div>
   );
 }
