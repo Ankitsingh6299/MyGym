b2d8e01d16928c86fed3cf12184af6cc833a10ae
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-09-27 18:39:03 +0530

    Testimonial data updated and join us section started

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './Join.css';
 function Join() {
   return (
-    <div>Join</div>
+    <div className='Join' id='join-us'>
+      <div className='left-j'>
+        <hr />
+          <div>
+            <span className='stroke-text'>READY TO</span>
+            <span> LEVEL UP</span>
+          </div>
+          <div>
+            <span>YOUR BODY</span>
+            <span className='stroke-text'> WITH US</span>
+          </div>
+      </div>
+      <div className='right-j'>
+        <div><span></span></div>
+        <form action='' className='email-container'>
+          <input type='email' name='user-email' placeholder='Enter your Email Address' />
+          <button className='btn btn-j'>Join Us</button>
+        </form>
+        </div>  
+    </div>
   )
 }
 
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,20 +1,20 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
+import ankit from "../assets/ankit.jpg";
+import amit from "../assets/amit.jpg";
 import image3 from "../assets/t-image3.jpg";
 
 export const testimonialsData = [
   {
-    image: image1,
+    image: ankit,
     review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+      "I made the right choice by choosing MyGym and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'Ankit Singh Chouhan',
+    status : 'Assistant System Engineer'
   },
   {
-    image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    image: amit,
+    review: 'I have been a part of this gym since last 5years and I am finally able to shape my body nto a better shape',
+    name: 'Amit',
+    status: 'IT Engineer'
   },
   {
     image : image3,
